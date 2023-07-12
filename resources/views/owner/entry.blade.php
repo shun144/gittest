7d67efae4434dc841e2e8a9a2ca3f520bfe63cf3
shunNel <noelgld13@gmail.com>
2023-07-12 15:51:42 +0900

    ユーザ連携画面修正

--- resources/views/owner/entry.blade.php
+++ resources/views/owner/entry.blade.php
@@ -33,26 +33,16 @@
                             <h3>LINE連携方法</h3>
                             <div class="paragraph col-12">
                                 <div class="descript" data-no="01">
-                                    <p>LINE連携ボタンをクリック。</p>
+                                    <p><span>LINE連携ボタン</span>をクリックします。</p>
                                 </div>
                                 <button form='lineAuthForm' type="submit">LINE連携</button>
                                
                             </div>
                             
-                            
                             <div class="paragraph col-12">
                                 <div class="descript" data-no="02">
-                                    <p>LINEのログイン画面が表示された場合はメールアドレスとパスワードを入力しログイン。
-                                    </p>
-                                </div>
-                                <div class="d-flex justify-content-center w-100">
-                                    <img class="card-img-top" src="{{ url(config('storage.user.image.entry').'/line_login_pick.png')}}" alt="Card image cap">
-                                </div>
-                            </div>
-                            <div class="paragraph col-12">
-                                <div class="descript" data-no="03">
                                     <p>表示された画面の
-                                        <span>1:1でLINE Notifyから通知を受け取る</span>を選択。
+                                        <span>1:1でLINE Notifyから通知を受け取る</span>を選択します。
                                     </p>
                                 </div>
                                 <div class="d-flex justify-content-center w-100">
@@ -60,21 +50,42 @@
                                 </div>
                             </div>
                             <div class="paragraph col-12">
-                                <div class="descript" data-no="04">
+                                <div class="descript" data-no="03">
                                     <p>
-                                        <span>同意して連携する</span>をクリック。
+                                        <span>同意して連携する</span>をクリックします。
                                     </p>
                                 </div>
                                 <div class="d-flex justify-content-center w-100">
                                     <img class="card-img-top" src="{{ url(config('storage.user.image.entry').'/agree.png')}}" alt="Card image cap">
                                 </div>
                             </div>
+
+                            <div class="paragraph col-12">
+                                <div class="descript" data-no="04">
+                                    <p>LINEのログイン画面が表示された場合はメールアドレスとパスワードを入力しログインします。
+                                    </p>
+                                </div>
+                                <div class="d-flex justify-content-center w-100">
+                                    <img class="card-img-top" src="{{ url(config('storage.user.image.entry').'/line_login_pick.png')}}" alt="Card image cap">
+                                </div>
+                            </div>
+
+                            <div class="paragraph col-12">
+                                <div class="descript" data-no="05">
+                                    <p>連携が完了すると、連携完了メッセージが通知されます。配信をお待ちください。
+                                    </p>
+                                </div>
+                                <div class="d-flex justify-content-center w-100">
+                                    <img class="card-img-top" src="{{ url(config('storage.user.image.entry').'/link_msg.png')}}" alt="Card image cap">
+                                </div>
+                            </div>
+
                         </div>
 
                         <div class="row section">
                             <h3>LINE通知について</h3>
                             <div class="paragraph col-12">
-                                <div class="descript" data-no="">
+                                <div class="descript" data-no="✔">
                                     <p>LINE Notifyの仕組みを利用しているため、メッセージは<span>LINE Notify</span>から通知されます。</p>
                                 </div>
                                 <div class="d-flex justify-content-center w-100">
@@ -87,62 +98,28 @@
                             <h3>LINE連携の解除</h3>
                             <div class="paragraph col-12">
                                 <div class="descript" data-no="01">
-                                    <p><a href="https://notify-bot.line.me/my/">LINE Notifyマイページ</a>へアクセスする。</p>
+                                    <p><a href="https://notify-bot.line.me/my/">LINE Notifyマイページ</a>へアクセスします。</p>
                                 </div>
                             </div>
                             <div class="paragraph col-12">
                                 <div class="descript" data-no="02">
-                                    <p>解除したいサービスの<span>解除ボタン</span>をクリック。</p>
+                                    <p>解除したいサービスの<span>解除ボタン</span>をクリックします。</p>
                                 </div>
                                 <div class="d-flex justify-content-center w-100">
-                                    <img class="card-img-top" src="{{ url(config('storage.user.image.entry').'/lift_bigger.png')}}" alt="Card image cap">
+                                    <img class="card-img-top" src="{{ url(config('storage.user.image.entry').'/lift_phone.png')}}" alt="Card image cap">
+                                </div>
+                            </div>
+                            <div class="paragraph col-12">
+                                <div class="descript" data-no="03">
+                                    <p>連携解除が完了すると、連携解除完了メッセージが通知されます。
+                                    </p>
+                                </div>
+                                <div class="d-flex justify-content-center w-100">
+                                    <img class="card-img-top" src="{{ url(config('storage.user.image.entry').'/unlink_msg.png')}}" alt="Card image cap">
                                 </div>
                             </div>
                         </div>
 
-                        {{-- <div class="row section section_last">
-                            <h3 class="regi_info_title">LINE連携の解除</h3>
-                            <div>
-                                <p>LINE連携を解除されたい場合は、<a href="https://notify-bot.line.me/my/">LINE Notifyのマイページ</a>へアクセスし、<span>解除</span>をクリックしてください。</p>
-                                
-                            </div>
-                            <div class="d-flex justify-content-center w-100">
-                                <img class="card-img-top entry_image" src="{{ url(config('storage.user.image.entry').'/lift.png')}}" alt="Card image cap">
-                            </div>
-                        </div> --}}
-
-
-                        {{-- <div class="row section">
-                            <h3>LINE連携時のログイン</h3>
-                            <div>
-                                <p><span>LINE連携方法</span>を実行し↓のような画面が出る場合は、メールアドレスとパスワードを入力し、ログインしてください。</p>
-                            </div>
-                            <div class="d-flex justify-content-center w-100">
-                                <img class="card-img-top" src="{{ url(config('storage.user.image.entry').'/line_login.png')}}" alt="Card image cap">
-                            </div>
-                        </div> --}}
-
-                        {{-- <div class="row section">
-                            <h3>LINE通知について</h3>
-                            <div>
-                                <p>LINE Notifyの仕組みを利用しているため、実際のLINE通知は↓のようにLINE Notifyから来ます。</p>
-                            </div>
-                            <div class="d-flex justify-content-center w-100">
-                                <img class="card-img-top" src="{{ url(config('storage.user.image.entry').'/notify.png')}}" alt="Card image cap">
-                            </div>
-                        </div> --}}
-
-
-                        {{-- <div class="row section section_last">
-                            <h3 class="regi_info_title">LINE連携の解除</h3>
-                            <div>
-                                <p>LINE連携を解除されたい場合は、<a href="https://notify-bot.line.me/my/">LINE Notifyのマイページ</a>へアクセスし、<span>解除</span>をクリックしてください。</p>
-                                
-                            </div>
-                            <div class="d-flex justify-content-center w-100">
-                                <img class="card-img-top entry_image" src="{{ url(config('storage.user.image.entry').'/lift.png')}}" alt="Card image cap">
-                            </div>
-                        </div> --}}
 
                     </div>
 
